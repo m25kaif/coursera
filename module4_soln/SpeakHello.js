6d923d9ceecc17b40a9ba3201cc03dc0d40c86ff
m25kaif <muhammedkaif25@gmail.com>
2020-06-01 10:08:17 +0530

    third

--- module4_soln/SpeakHello.js
+++ module4_soln/SpeakHello.js
@@ -6,12 +6,3 @@
     };
     window.helloSpeaker = helloSpeaker;
 })(window);
-
-(function(window) {
-    var byeSpeaker = new Object();
-    var speakWord = "Good Bye";
-    byeSpeaker.speak = function speak(name) {
-        console.log(speakWord + " " + name);
-    };
-    window.byeSpeaker = byeSpeaker;
-})(window);
